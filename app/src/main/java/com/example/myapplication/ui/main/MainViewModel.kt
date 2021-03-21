e30bb6afbb1dd14672dbcf40c5dbbba33e27b327
Nikita Kladov <vastausf@gmail.com>
2021-03-21 12:43:04 +0300

    Init commit

---
+++ app/src/main/java/com/example/myapplication/ui/main/MainViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication.ui.main
+
+import androidx.lifecycle.ViewModel
+
+//class Greeter() : GreeterGrpc.GreeterImplBase() {
+//    override suspend fun sayHello(request: GreeterOuterClass.HelloRequest): GreeterOuterClass.HelloReply {
+//        return super.sayHello(request)
+//    }
+//}
+
+class MainViewModel : ViewModel() {
+
+    // TODO: Implement the ViewModel
+}
